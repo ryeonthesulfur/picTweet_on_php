78ce622ef59b3698a9e2e132c1ef212ad7fd5390
ryeonthesulfur <perplexing2004@gmail.com>
2026-05-05 22:51:00 +0900

    Tweets コントローラーの編集
    ・Auth ファサードの使用宣言
    ・create アクション記述（新規投稿画面）
    ・store アクション記述（投稿内容の保存）
    ・バリデーションの記述

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Tweet;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class TweetController extends Controller
@@ -12,5 +13,27 @@ class TweetController extends Controller
     {
         $tweets = Tweet::all();
         return view('tweets.index', compact('tweets'));
+    }
+
+    // 新規投稿画面の表示
+    public function create()
+    {
+        return view('tweets.create');
+    }
+
+    // 新規投稿の保存
+    public function store(Request $request)
+    {
+        $request->validate([
+            'text' => ['required'],
+        ]);
+
+        Tweet::create([
+            'text' => $request->input('text'),
+            'image' => $request->hasFile('image') ? $request->file('image')->store('images','public') : null,   
+            'user_id' => Auth::id(),
+        ]);
+
+        return redirect('/');
     }
 }
