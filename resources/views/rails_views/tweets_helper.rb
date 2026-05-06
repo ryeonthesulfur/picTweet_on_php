0dd19620aac7e1869ffba849db01188485393058
ryeonthesulfur <perplexing2004@gmail.com>
2026-05-06 14:35:54 +0900

    不要ファイルの削除

--- resources/views/rails_views/tweets_helper.rb
+++
@@ -1,9 +0,0 @@
-module TweetsHelper
-    def tweet_lists(tweets)
-        html = ''
-        tweets.each do |tweet|
-            html += render(partial: 'tweet',locals: {tweet: tweet})
-        end
-        return raw(html)
-    end
-end
