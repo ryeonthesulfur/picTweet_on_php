45c672e20b00f2ba22ccbca8938ac43025df4e9f
ryeonthesulfur <perplexing2004@gmail.com>
2026-05-06 17:40:46 +0900

    Prefix パス修正

--- resources/views/tweets/_form.blade.php
+++ resources/views/tweets/_form.blade.php
@@ -1,4 +1,4 @@
-  <form action="{{ route('tweet.store') }}" method="POST" enctype="multipart/form-data">              
+  <form action="{{ route('tweets.store') }}" method="POST" enctype="multipart/form-data">              
       @csrf                                                                       
       <input type="file" name="image">
       <textarea name="text" placeholder="text" rows="10"></textarea>              
