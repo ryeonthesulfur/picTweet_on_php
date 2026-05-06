2ab9fdedcb5eb300eafde88ca8c6f5b4cc31a6fa
ryeonthesulfur <perplexing2004@gmail.com>
2026-05-06 14:28:41 +0900

    コントローラーの編集
    ・edit, update, destroy メソッド記述
    ・各メソッド内におけるバリデーションの記述

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -36,4 +36,47 @@ class TweetController extends Controller
 
         return redirect('/');
     }
+
+    // 編集画面の表示
+    public function edit($id)
+    {
+        $tweet = Tweet::find($id);
+        if (Auth::id() !== $tweet->user_id) {
+            return redirect('/');  // 不正アクセスを防止するため、トップ画面にリダイレクト
+        }
+
+        return view('tweets.edit', compact('tweet'));
+    }
+
+    // 編集内容の保存
+    public function update(Request $request, $id)
+    {
+        $tweet = Tweet::find($id);
+        if (Auth::id() !== $tweet->user_id) {
+            return redirect('/');  // 不正アクセスを防止するため、本人じゃなかったらトップ画面にリダイレクト
+        }
+        
+        $request->validate([
+            'text' => ['required'],
+        ]);
+
+        $tweet->update([
+            'text' => $request->input('text'),
+            'image' => $request->hasFile('image') ? $request->file('image')->store('images','public') : $tweet->image,
+        ]);
+
+        return redirect('/');
+}
+
+    // 投稿の削除
+    public function destroy($id)
+    {
+        $tweet = Tweet::find($id);
+        if (Auth::id() !== $tweet->user_id) {
+            return redirect('/');  // 不正アクセスを防止するため、本人じゃなかったらトップ画面にリダイレクト
+        }
+
+        $tweet->delete();
+        return redirect('/');
+    }
 }
